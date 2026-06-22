748d70085d9a102b8ec5fd10b359f90977a9e479
diegoparradoc96 <diegoparrado.cardozo@gmail.com>
2026-06-22 18:24:36 -0500

    feat: develop the projects section

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod'
+import { glob } from 'astro/loaders';
+
+const projectsCollection = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/projects' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    link: z.url().optional(),
+  }),
+});
+
+export const collections = {
+  'projects': projectsCollection,
+};
